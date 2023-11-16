94cde9cf27dbbdf8a523b2d1b83817a7b7ea898e
ankita tripathy <in_kumarit@m1.com.sg>
2023-11-16 22:07:41 +0530

    Task 3: Undo add and remove from reading list

--- libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
@@ -1,22 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, Observable,of  } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
-
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { SharedTestingModule, createBook, createReadingListItem } from '@tmo/shared/testing';
 import { ReadingListEffects } from './reading-list.effects';
 import * as ReadingListActions from './reading-list.actions';
-import { BookConstant } from '@tmo/shared/models';
+import { BookConstant,Book, ReadingListItem, } from '@tmo/shared/models';
+import { Action } from '@ngrx/store';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('Reading list effects', () => {
-  let actions: ReplaySubject<any>;
+  let actions: Observable<Action>;
   let effects: ReadingListEffects;
   let httpMock: HttpTestingController;
+  const error:any = new ErrorEvent('error' );
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SharedTestingModule],
+      imports: [SharedTestingModule, MatSnackBarModule],
       providers: [
         ReadingListEffects,
         provideMockActions(() => actions),
@@ -40,5 +42,127 @@ describe('Reading list effects', () => {
       });
       httpMock.expectOne(BookConstant.API.READING_LIST_API).flush([]);
     });
+    it('should dispatch confirmedAddToReadingList when book is added to the reading list', (done) => {
+      const book: Book = { ...createBook('A'), isAdded: true };
+      actions = of(
+        ReadingListActions.addToReadingList({ book, showSnackBar: true })
+      );
+  
+      effects.addBook$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedAddToReadingList({
+            book,
+            showSnackBar: true
+          })
+        );
+        done();
+      });
+  
+      httpMock
+        .expectOne(`${BookConstant.API.READING_LIST_API}`)
+        .flush([book]);
+    });
+  
+    it('should dispatch failedAddToReadingList when api throws an error', (done) => {
+      const book: Book = { ...createBook('A'), isAdded: false };
+      actions = of(
+        ReadingListActions.addToReadingList({ book: book, showSnackBar: false })
+      );
+      const result = ReadingListActions.failedAddToReadingList(
+        new ErrorEvent('error')
+      );
+  
+      effects.addBook$.subscribe((action) => {
+        expect(action.type).toEqual(result.type);
+        done();
+      });
+  
+      httpMock
+        .expectOne(`${BookConstant.API.READING_LIST_API}`)
+        .error(new ErrorEvent('error'));
+    });
+    it('should dispatch confirmedRemoveFromReadingList  from the reading list', (done) => {
+      const item = createReadingListItem('A');
+      actions = of(
+        ReadingListActions.removeFromReadingList({
+          item: item,
+          showSnackBar: true
+        })
+      );
+  
+      effects.removeBook$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedRemoveFromReadingList({
+            item: item,
+            showSnackBar: true
+          })
+        );
+        done();
+      });
+  
+      httpMock
+        .expectOne(`${BookConstant.API.READING_LIST_API}/${item.bookId}`)
+        .flush([item]);
+    });
+  
+    it('should dispatch failedRemoveFromReadingList when api throws an error', (done) => {
+      const item: ReadingListItem = createReadingListItem('A');
+      actions = of(
+        ReadingListActions.removeFromReadingList({ item, showSnackBar: true })
+      );
+      const outcome = ReadingListActions.failedRemoveFromReadingList(
+        new ErrorEvent('error')
+      );
+  
+      effects.removeBook$.subscribe((action) => {
+        expect(action.type).toEqual(outcome.type);
+        done();
+      });
+  
+      httpMock
+        .expectOne(`${BookConstant.API.READING_LIST_API}/${item.bookId}`)
+        .error(new ErrorEvent('error'));
+    });
+    it('should undo addition of book when showSnackbar action is dispatched', (done) => {
+      const book: Book = { ...createBook('A'), isAdded: true };
+      actions = of(
+        ReadingListActions.confirmedAddToReadingList({
+          book: book,
+          showSnackBar: true
+        })
+      );
+  
+      effects.undoAddBook$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.showSnackBar({
+            actionType: BookConstant.SNACKBAR_CONSTANTS.ADD,
+            item: { bookId: book.id, ...book }
+          })
+        );
+        done();
+      });
+    });
+    it('should undo removal of book when showSnackbar action is dispatched', (done) => {
+      const item: ReadingListItem = createReadingListItem('A');
+      actions = of(
+        ReadingListActions.confirmedRemoveFromReadingList({
+          item: item,
+          showSnackBar: true
+        })
+      );
+  
+      effects.undoRemoveBook$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.showSnackBar({
+            actionType: BookConstant.SNACKBAR_CONSTANTS.REMOVE,
+            item: action.item
+          })
+        );
+        done();
+      });
+    });
+
   });
 });
+
+
--- libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.spec.ts
@@ -1,13 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SharedTestingModule } from '@tmo/shared/testing';
-
+import {
+  createReadingListItem,
+  SharedTestingModule,
+} from '@tmo/shared/testing';
 import { ReadingListComponent } from './reading-list.component';
 import { BooksFeatureModule } from '@tmo/books/feature';
+import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { BookConstant } from '@tmo/shared/models';
 
 describe('ReadingListComponent', () => {
   let component: ReadingListComponent;
   let fixture: ComponentFixture<ReadingListComponent>;
-
+  let store: MockStore;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [BooksFeatureModule, SharedTestingModule]
@@ -23,4 +28,45 @@ describe('ReadingListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should show the reading list when books are added', () => {
+    const bookCover = fixture.nativeElement.querySelector(
+      '[data-testing="book-cover"]'
+    );
+    const bookTitle = fixture.nativeElement.querySelector(
+      '[data-testing="book-title"]'
+    );
+    const bookAuthorName = fixture.nativeElement.querySelector(
+      '[data-testing="book-author-name"]'
+    );
+    expect(bookCover).toBeTruthy();
+    expect(bookTitle.innerHTML.trim()).toBe('Book testing');
+    expect(bookAuthorName.innerHTML.trim()).toBe('Author testing');
+  });
+ 
+  it('should dispatch removeFromReadingList action when remove button is clicked', () => {
+    const bookItem = { ...createReadingListItem('testing'), isAdded: true };
+    const removeButton = fixture.nativeElement.querySelector(
+      '[data-testing="remove-book"]'
+    ) as HTMLButtonElement;
+    removeButton.click();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      removeFromReadingList({
+        item: bookItem, showSnackBar: true
+      })
+    );
+  });
+ 
+  it('should show the empty text when no books are added', () => {
+    store.overrideSelector(getReadingList, []);
+ 
+    store.refreshState();
+    fixture.detectChanges();
+    const emptyText = fixture.nativeElement.querySelector(
+      '[data-testing="empty-text"]'
+    );
+    expect(emptyText.innerHTML.trim()).toBe(
+      BookConstant.READING_LIST.EMPTY_LIST_TEXT
+    );
+  });
 });
+
